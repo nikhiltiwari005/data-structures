3157b4f3fb4cd4d0519f62d804dcb6e85e189eb7
Nikhil Tiwari <nikhiltiwari005@gmail.com>
2025-01-10 19:24:18 +0530

    memory profiler added

--- python/time-complexity/01-linear.py
+++ python/time-complexity/01-linear.py
@@ -1,10 +1,14 @@
+from memory_profiler import profile
+
+print("Linear TX \n")
+
+names = ["john", "sam", "raj"]
+
+@profile
+def printNames(list):
+    print("No of persons", len(list))
+    for index, name in enumerate(names):
+        print(index + 1, str.capitalize(name))
 
 
-print("Hi there \n")
-
-names = ["nikhil", "swati", "nick"]
-
-for index, name in enumerate(names):
-    print(index, str.capitalize(name))
-
-print(index + 1, str.capitalize(name))
+printNames(names)
